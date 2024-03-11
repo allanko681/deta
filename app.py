084d04a3ad91c71d94c811ad110503b2966a3a43
allanko681 <162938688+allanko681@users.noreply.github.com>
2024-03-10 21:50:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,9 @@ def d():
     video = YouTube(url)
     stream = video.streams.filter(only_audio=True).first()
     filename = stream.default_filename
-    extension = stream.extension
     filepath = f"/opt/render/project/src/{filename}"
     stream.download(filepath)
+    return send_file(filepath, as_attachment=True)
     
-    # Cambiar la extensión del archivo a mp3
-    new_filepath = f"{filepath.rsplit('.', 1)[0]}.mp3"
-    os.rename(filepath, new_filepath)
 if __name__ == '__main__':
     app.run
