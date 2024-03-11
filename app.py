0cb10f3609f13d9923aef64f214438fcb10ce272
allanko681 <162938688+allanko681@users.noreply.github.com>
2024-03-10 22:10:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
-import os
 
 app = Flask(__name__)
 
@@ -13,16 +12,9 @@ def d():
     url = request.form['url']
     video = YouTube(url)
     stream = video.streams.filter(only_audio=True).first()
-    filename = stream.default_filename
-    filepath = f"/opt/render/project/src/{filename}"
-    stream.download(filepath)
-    
-    # Cambiar la extensión del archivo a mp3 si no lo es ya
-    if not filepath.endswith(".mp3"):
-        mp3_filepath = f"{filepath}.mp3"
-        os.rename(filepath, mp3_filepath)
-        filepath = mp3_filepath
-    
-    return send_file(filepath, as_attachment=True)
+    filename=f"{video.title}.mp3"
+    stream.download(filename=f"{video.title}.mp3")
+    filename=f"{video.title}.mp3"
+    return send_file(filename, as_attachment=True)
 if __name__ == '__main__':
     app.run
