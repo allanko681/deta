f9a1099bcaa8eb478589daaf2aff3f212cfbd61b
allanko681 <162938688+allanko681@users.noreply.github.com>
2024-03-10 20:06:31 -0600

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+
+app = Flask(__name__)
+
+@app.route('/')
+def route():
+    return render_template('r.html')
+
+@app.route('/d', methods = ['POST'])
+def d():
+    url = request.form['url']
+    video = YouTube(url)
+    stream = video.streams.filter(only_audio=True).first()
+    return stream.download(filename=f"{video.title}.mp3")
+if __name__ == '__main__':
+    app.run(host='localhost')
